c004ca75d6570c0420534c9231e4ee5e25ca8ed3
jlard <jaxtonlard@gmail.com>
2017-12-31 20:47:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	Dialogflow: '<Enter your Dialogflow token here>',
-	Discord: '<Enter your discord token here>'
+	Dialogflow: '<104496e375784e6db17c22b4180b9d4f>',
+	Discord: '<397195032916131865>'
 }
